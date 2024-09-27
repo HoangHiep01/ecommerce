470fe164c28a615fba40e46303141e1d588611c1
HoangHiep <nhoanghiep2001@gmail.com>
2024-09-27 14:07:29 +0700

    feat: create role decorator

---
+++ src/auth/decorators/role.decorator.ts
@@ -0,0 +1,5 @@
+import { UserRole } from '../../users/entities/user.entity';
+import { SetMetadata } from '@nestjs/common';
+
+export const ROLES_KEY = 'role';
+export const Role = (role: UserRole) => SetMetadata(ROLES_KEY, role);
