1fe49c8ad6ec65fbc92ceac9976bccc9b0e35e3e
HoangHiep <nhoanghiep2001@gmail.com>
2024-09-29 00:39:03 +0700

    refactor: declare typeorm config

--- db/data.source.ts
+++ db/data.source.ts
@@ -1,12 +1,37 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
 import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => {
+    return {
+      type: 'postgres',
+      host: configService.get<string>('database.host'),
+      port: configService.get<number>('database.port'),
+      username: configService.get<string>('database.username'),
+      password: configService.get<string>('database.password'),
+      database: configService.get<string>('database.name'),
+      entities: ['dist/**/*.entity.js'],
+      migrations: ['dist/db/migrations/*.js'],
+      synchronize: false,
+    };
+  },
+};
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'appuser',
-  password: '123456',
-  database: 'ecommercedb',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
   synchronize: false,
