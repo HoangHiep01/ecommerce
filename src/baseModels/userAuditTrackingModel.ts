38bfacc506299e06a413deb6c70e051dbc7ffc52
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-02 10:45:11 +0700

    fix(relation): one user can create many record

--- src/baseModels/userAuditTrackingModel.ts
+++ src/baseModels/userAuditTrackingModel.ts
@@ -2,8 +2,9 @@ import {
   CreateDateColumn,
   DeleteDateColumn,
   UpdateDateColumn,
-  OneToOne,
+  ManyToOne,
   Column,
+  JoinColumn,
 } from 'typeorm';
 import { BaseModel } from './baseEntity';
 import { User } from '../users/entities/user.entity';
@@ -12,31 +13,22 @@ export class UserAuditTrackingModel extends BaseModel {
   @CreateDateColumn()
   createAt!: Date;
 
-  @OneToOne(() => User)
-  @Column({
-    type: 'integer',
-    nullable: false,
-  })
-  createBy: number;
+  @ManyToOne(() => User)
+  @JoinColumn()
+  createBy!: number;
 
   @UpdateDateColumn()
   updateAt?: Date;
 
-  @OneToOne(() => User)
-  @Column({
-    type: 'integer',
-    nullable: true,
-  })
+  @ManyToOne(() => User)
+  @JoinColumn()
   updateBy: number;
 
   @DeleteDateColumn()
   deletedAt?: Date;
 
-  @OneToOne(() => User)
-  @Column({
-    type: 'integer',
-    nullable: true,
-  })
+  @ManyToOne(() => User)
+  @JoinColumn()
   deleteBy: number;
 
   @Column({ default: false })
