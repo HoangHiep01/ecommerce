bbe7572d07784f38e6d97a0f912be5eb0527de31
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-02 10:32:30 +0700

    feat(): implement business logic

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,22 +1,68 @@
-import { Injectable } from '@nestjs/common';
-// import { CreateProductDto } from './dto/create-product.dto';
-// import { UpdateProductDto } from './dto/update-product.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Request } from 'express';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
-  // create(createProductDto: CreateProductDto) {
-  //   return 'This action adds a new product';
-  // }
-  // findAll() {
-  //   return `This action returns all product`;
-  // }
-  // findOne(id: number) {
-  //   return `This action returns a #${id} product`;
-  // }
-  // update(id: number, updateProductDto: UpdateProductDto) {
-  //   return `This action updates a #${id} product`;
-  // }
-  // remove(id: number) {
-  //   return `This action removes a #${id} product`;
-  // }
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
+
+  async create(
+    createProductDto: CreateProductDto,
+    request: Request,
+  ): Promise<Product> {
+    const product = new Product();
+
+    product.name = createProductDto.name;
+    product.description = createProductDto.description;
+    product.price = createProductDto.price;
+    // First one created will be first one updated.
+    product.createBy = request['user'].sub;
+    product.updateBy = request['user'].sub;
+    // createAt and updateAt will be auto add Date.now() in table.
+    return await this.productsRepository.save(product);
+  }
+  async findAll(): Promise<Product[]> {
+    return await this.productsRepository.findBy({
+      isDelete: false,
+    });
+  }
+  async findOne(id: number): Promise<Product | undefined> {
+    const result = await this.productsRepository.findOneBy({
+      id: id,
+      isDelete: false,
+    });
+    if (!result) {
+      throw new NotFoundException('Product not found.');
+    }
+    return result;
+  }
+  async update(
+    id: number,
+    updateProductDto: UpdateProductDto,
+    request: Request,
+  ): Promise<any> {
+    const product = await this.productsRepository.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException('Product not found.');
+    }
+    await this.productsRepository.update(id, updateProductDto);
+    await this.productsRepository.update(id, {
+      updateBy: request['user'].sub,
+      updateAt: new Date(),
+    });
+  }
+  async remove(id: number, request: Request): Promise<void> {
+    const product = await this.findOne(id);
+    product.isDelete = true;
+    product.deleteBy = request['user'].sub;
+    product.deletedAt = new Date();
+    await this.productsRepository.update(id, product);
+  }
 }
