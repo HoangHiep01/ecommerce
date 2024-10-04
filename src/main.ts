c5f67b1e5532e1dbac40724864cbf16482555cfb
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-04 10:08:43 +0700

    feat(): implementation swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
+
+  setupSwagger(app);
+
   await app.listen(configService.get<number>('port'));
 }
 bootstrap();
