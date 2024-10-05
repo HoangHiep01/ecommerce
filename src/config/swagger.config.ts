a01aad431f9f4ffec7a1c9bde4fcbb28d1266bd7
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-06 02:53:28 +0700

    chore(): add bearer auth options

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -5,23 +5,23 @@ import {
 } from '@nestjs/swagger';
 import type { INestApplication } from '@nestjs/common';
 
-// const bearerAuthOption = {{
-//         type: 'http',
-//         scheme: 'bearer',
-//         bearerFormat: 'JWT',
-//         name: 'JWT',
-//         description: 'Enter JWT token',
-//         in: 'header',
-//     },
-//     'JWT-auth'};
-
 export function setupSwagger(app: INestApplication): void {
   const documentBuiler = new DocumentBuilder()
     .setTitle('Ecommerce example')
     .setDescription('The ecommerce API description')
     .setVersion('1.0')
     .addTag('Ecommerce')
-    .addBearerAuth()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
     .build();
 
   const documentBuilerOptions: SwaggerDocumentOptions = {
