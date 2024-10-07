fe2a832b8febdd75c264f67b17e5716a14f14e85
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-07 17:39:27 +0700

    feat(): declare customers table

--- src/modules/customers/entities/customer.entity.ts
+++ src/modules/customers/entities/customer.entity.ts
@@ -1,1 +1,30 @@
-export class Customer {}
+import { Entity, Column } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { UserAuditTrackingModel } from '../../../baseModels/userAuditTrackingModel';
+
+@Entity('customers')
+export class Customer extends UserAuditTrackingModel {
+  @ApiProperty({
+    description: 'provide full name of customer.',
+  })
+  @Column('varchar', { length: 150 })
+  name: string;
+
+  @ApiProperty({
+    description: 'provide customer address for shipping.',
+  })
+  @Column('text')
+  address: string;
+
+  @ApiProperty({
+    description: 'provide email.',
+  })
+  @Column('varchar', { length: 256 })
+  email: string;
+
+  @ApiProperty({
+    description: 'provide phoneumber.',
+  })
+  @Column({ nullable: false })
+  phoneNumber?: string;
+}
