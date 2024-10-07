bb654b8a6aaf2676dc71d5a7c574d0367ebb30a8
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-07 17:41:20 +0700

    feat(): declare field to create customer

--- src/modules/customers/dto/create-customer.dto.ts
+++ src/modules/customers/dto/create-customer.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCustomerDto {}
+import { IsString, IsNotEmpty, IsNumberString, IsEmail } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly address;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  readonly phoneNumber;
+}
