62af4a4dcbaf834fa98b41ba6edb8f5174e5da23
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-07 17:40:26 +0700

    feat(): declare fields for customer update

--- src/modules/customers/dto/update-customer.dto.ts
+++ src/modules/customers/dto/update-customer.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCustomerDto } from './create-customer.dto';
+import { Transform } from 'class-transformer';
+import { IsOptional, IsBoolean } from 'class-validator';
 
-export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {
+  @IsBoolean()
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (value === 'true') return true;
+    if (value === 'false') return false;
+    return value;
+  })
+  isDelete;
+}
