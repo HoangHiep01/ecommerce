abadfa72aaf9c58aff9441383d92faf8eeecefbc
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-08 21:09:56 +0700

    feat(): import typeORM and export service

--- src/modules/inventory/inventory.module.ts
+++ src/modules/inventory/inventory.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Inventory } from './entities/inventory.entity';
+import { ProductModule } from '../product/product.module';
 
 @Module({
+  imports: [ProductModule, TypeOrmModule.forFeature([Inventory])],
   controllers: [InventoryController],
   providers: [InventoryService],
+  exports: [InventoryService],
 })
 export class InventoryModule {}
