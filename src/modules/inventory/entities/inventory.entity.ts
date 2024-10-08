3050f7e5ced69142186a80a10d0ca02152814e35
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-08 21:12:59 +0700

    feat(): declare inventory fields table

--- src/modules/inventory/entities/inventory.entity.ts
+++ src/modules/inventory/entities/inventory.entity.ts
@@ -1,1 +1,15 @@
-export class Inventory {}
+import { Entity, Column, OneToOne, JoinColumn } from 'typeorm';
+import { UserAuditTrackingModel } from '../../../baseModels/userAuditTrackingModel';
+import { Product } from '../../product/entities/product.entity';
+
+@Entity('inventory')
+export class Inventory extends UserAuditTrackingModel {
+  @Column({
+    default: 0,
+  })
+  quantity: number;
+
+  @OneToOne(() => Product)
+  @JoinColumn()
+  productId: number;
+}
