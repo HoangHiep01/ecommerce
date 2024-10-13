e5f23ae20c54918a5b06bef51000969ec224e833
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-14 02:45:10 +0700

    feat(): declare order route api

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -5,38 +5,58 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
+  Req,
+  DefaultValuePipe,
+  ParseIntPipe,
+  Query,
 } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { Request } from 'express';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { ApiDocument } from '../../decorators/document.decorator';
 
+@ApiTags('Order')
+@ApiBearerAuth('JWT-auth')
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
+  @ApiDocument('Create a customer order.', 'New order was created.')
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderService.create(createOrderDto);
+  create(@Body() createOrderDto: CreateOrderDto, @Req() request: Request) {
+    return this.orderService.create(createOrderDto, request);
   }
 
+  @ApiDocument('Get list orders.', 'A list orders')
   @Get()
-  findAll() {
-    return this.orderService.findAll();
+  findAll(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
+  ) {
+    limit = limit > 100 ? 100 : limit;
+    return this.orderService.findAll({ page, limit });
   }
 
+  @ApiDocument('Get specific order by id.', 'A order')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.orderService.findOne(+id);
   }
 
+  @ApiDocument('Update state order by id.', 'State was updated')
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.orderService.update(+id, updateOrderDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateOrderDto: UpdateOrderDto,
+    @Req() request: Request,
+  ) {
+    return this.orderService.update(+id, updateOrderDto, request);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.orderService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.orderService.remove(+id);
+  // }
 }
