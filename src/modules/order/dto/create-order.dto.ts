1ba2382f1e64deae1f08dbe7c05fa0c2ccbbeaa5
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-14 02:40:48 +0700

    feat(): declare data transfer when call api order

--- src/modules/order/dto/create-order.dto.ts
+++ src/modules/order/dto/create-order.dto.ts
@@ -1,1 +1,11 @@
-export class CreateOrderDto {}
+import { IsNotEmpty, IsNumber, IsEnum } from 'class-validator';
+import { OrderState } from '../../../constants/order-state';
+
+export class CreateOrderDto {
+  @IsNotEmpty()
+  @IsNumber()
+  cartId;
+
+  @IsEnum(OrderState)
+  orderState;
+}
