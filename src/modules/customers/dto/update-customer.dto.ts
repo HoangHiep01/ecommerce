7f8ab5873e63b7202618afb81738e48226da7306
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-14 01:58:21 +0700

    fix(): use PartialType to make code more abstractive

--- src/modules/customers/dto/update-customer.dto.ts
+++ src/modules/customers/dto/update-customer.dto.ts
@@ -1,15 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCustomerDto } from './create-customer.dto';
-import { Transform } from 'class-transformer';
-import { IsOptional, IsBoolean } from 'class-validator';
 
-export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {
-  @IsBoolean()
-  @IsOptional()
-  @Transform(({ value }) => {
-    if (value === 'true') return true;
-    if (value === 'false') return false;
-    return value;
-  })
-  isDelete;
-}
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
