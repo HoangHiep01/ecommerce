0cdd4bf77a8d30b5d7c40cfece7c680e85b758cd
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-14 01:35:56 +0700

    fix(): update come with database refactor

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,10 +1,16 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import {
+  paginate,
+  Pagination,
+  IPaginationOptions,
+} from 'nestjs-typeorm-paginate';
 import { CreateUserDto } from './dto/create-user.dto';
 // import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import * as bcryptjs from 'bcryptjs';
+import { genenateReturnObject } from '../../constants/return-object';
 
 @Injectable()
 export class UsersService {
@@ -13,42 +19,77 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const user = new User();
+  async createUser(createUserDto: CreateUserDto): Promise<object> {
+    try {
+      const user = new User();
+      const isUserNameExist = await this.findOneByUserName(
+        createUserDto.userName,
+      );
+      const isEmailExist = await this.findOneByEmail(createUserDto.email);
 
-    // Check username or email is exist?
-    const isUsernameExist = await this.findOneByUsername(
-      createUserDto.username,
-    );
-    const isEmailExist = await this.findOneByEmail(createUserDto.email);
-    if (isUsernameExist || isEmailExist) {
-      throw new BadRequestException('Username or email already taken.');
-    }
+      if (isUserNameExist || isEmailExist) {
+        return genenateReturnObject(
+          400,
+          {},
+          'Username or email already taken.',
+        );
+      }
 
-    user.username = createUserDto.username;
-    user.email = createUserDto.email;
-    if (createUserDto.phoneNumber) {
-      user.phoneNumber = createUserDto.phoneNumber;
-    }
-    if (createUserDto.role) {
-      user.role = createUserDto.role;
-    }
-    // hash password
-    user.password = await bcryptjs.hash(createUserDto.password, 10);
+      user.userName = createUserDto.userName;
+      user.email = createUserDto.email;
+      if (createUserDto.phoneNumber) {
+        user.phoneNumber = createUserDto.phoneNumber;
+      }
+      if (createUserDto.role) {
+        user.role = createUserDto.role;
+      }
+      // hash password
+      user.password = await bcryptjs.hash(createUserDto.password, 10);
 
-    return await this.usersRepository.save(user);
+      const data = await this.usersRepository.save(user);
+      delete data.password;
+
+      return genenateReturnObject(200, data);
+    } catch (e) {
+      return genenateReturnObject(400, {}, (e as Error).message);
+    }
   }
 
-  async findAll(): Promise<User[]> {
-    return this.usersRepository.find();
+  async findAll(options: IPaginationOptions): Promise<Pagination<User>> {
+    const query = this.usersRepository.createQueryBuilder('users');
+    return paginate<User>(query, options);
   }
 
-  async findOneByUsername(username: string): Promise<User | undefined> {
-    return this.usersRepository.findOneBy({ username });
+  async findOneByUserName(userName: string): Promise<User | undefined> {
+    try {
+      return this.usersRepository.findOneBy({ userName });
+    } catch (e) {
+      console.log(e);
+      return undefined;
+    }
   }
 
   async findOneByEmail(email: string): Promise<User | undefined> {
-    return this.usersRepository.findOneBy({ email });
+    try {
+      return this.usersRepository.findOneBy({ email });
+    } catch (e) {
+      console.log(e);
+      return undefined;
+    }
+  }
+
+  async findPasswordByUserName(userName: string): Promise<User | undefined> {
+    try {
+      return await this.usersRepository
+        .createQueryBuilder('users')
+        .select('users.userName', 'userName')
+        .addSelect('users.password')
+        .where('users.userName = :userName', { userName: userName })
+        .getOne();
+    } catch (e) {
+      console.log(e);
+      return undefined;
+    }
   }
 
   // update(id: number, updateUserDto: UpdateUserDto) {
