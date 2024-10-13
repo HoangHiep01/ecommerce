b79807a50075bccb4969b7f130afe0202fb681ab
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-14 02:54:19 +0700

    fix(): update code logic work with cart

--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
-import { Repository } from 'typeorm';
+import { Repository, Equal, Not, IsNull } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { paginate, IPaginationOptions } from 'nestjs-typeorm-paginate';
 import { CreateCartDto } from './dto/create-cart.dto';
@@ -11,6 +11,7 @@ import { Cart } from './entities/cart.entity';
 import { CartItem } from './entities/cart-item.entity';
 import { CustomersService } from '../customers/customers.service';
 import { InventoryService } from '../inventory/inventory.service';
+import { genenateReturnObject } from '../../constants/return-object';
 
 @Injectable()
 export class CartService {
@@ -27,33 +28,36 @@ export class CartService {
     createCartDto: CreateCartDto,
     request: Request,
   ): Promise<object> {
-    if (!(await this.customersService.findOne(createCartDto.customer))) {
-      return { error: { message: 'Customer not found.' } };
+    try {
+      const customer = await this.customersService.findOne(
+        createCartDto.customerId,
+      );
+      if (customer['statusCode'] != 200) {
+        return genenateReturnObject(400, {}, 'Customer not found');
+      }
+
+      // https://github.com/typeorm/typeorm/issues/2500
+      const existCart = await this.cartRepository.findOne({
+        where: { customer: Equal(createCartDto.customerId) },
+      });
+      if (existCart) {
+        return genenateReturnObject(
+          201,
+          existCart,
+          'Customer already has a cart.',
+        );
+      }
+
+      const cart = new Cart();
+      cart.customer = createCartDto.customerId;
+      cart.createdBy = request['user'].sub;
+      cart.updatedBy = request['user'].sub;
+      const data = await this.cartRepository.save(cart);
+
+      return genenateReturnObject(200, data);
+    } catch (e) {
+      return genenateReturnObject(e.statusCode, {}, (e as Error).message);
     }
-
-    if (
-      await this.cartRepository.findOneBy({
-        customer: createCartDto.customer,
-      })
-    ) {
-      return { error: { message: 'Customer already has a cart.' } };
-    }
-
-    const cart = new Cart();
-    cart.customer = createCartDto.customer;
-    cart.createBy = request['user'].sub;
-    cart.updateBy = request['user'].sub;
-    const isCartSaved = await this.cartRepository.save(cart);
-
-    if (isCartSaved) {
-      return {
-        data: {
-          isCartSaved,
-        },
-      };
-    }
-
-    return { error: { message: 'Can not create cart.' } };
   }
 
   async add(
@@ -61,55 +65,77 @@ export class CartService {
     request: Request,
     addItemDto: AddItemDto,
   ): Promise<object> {
-    if (!(await this.cartRepository.findOneBy({ id: cartId }))) {
-      return { error: { message: 'Cart not found.' } };
-    }
-    if (!(await this.inventoryService.findOne(addItemDto.inventoryId))) {
-      return { error: { message: 'Can not find product in inventory.' } };
-    }
+    try {
+      const isCartExist = await this.cartRepository.findOneBy({
+        id: Equal(cartId),
+      });
+      if (!isCartExist) {
+        return genenateReturnObject(404, {}, 'Cart not found.');
+      }
 
-    const isItemInCart = await this.cartItemRepository.findOneBy({
-      cart: cartId,
-      inventory: addItemDto.inventoryId,
-    });
-    if (isItemInCart) {
-      isItemInCart.quantity += addItemDto.quantity;
-      const updateItem = await this.cartItemRepository.save(isItemInCart);
-      return { data: updateItem };
+      const isItemExist = await this.inventoryService.findOne(
+        addItemDto.inventoryId,
+      );
+      if (isItemExist['statusCode'] != 200) {
+        return genenateReturnObject(404, {}, 'Product not found in inventory.');
+      }
+
+      const isItemInCart = await this.cartItemRepository.findOne({
+        where: {
+          cart: Equal(cartId),
+          inventory: Equal(addItemDto.inventoryId),
+        },
+      });
+      if (isItemInCart) {
+        isItemInCart.quantity += addItemDto.quantity;
+        const updateItem = await this.cartItemRepository.save(isItemInCart);
+        return genenateReturnObject(
+          201,
+          updateItem,
+          'Product already in cart, quantity added.',
+        );
+      }
+      const item = new CartItem();
+      item.cart = isCartExist;
+      item.inventory = addItemDto.inventoryId;
+      item.quantity = addItemDto.quantity;
+
+      const addItem = await this.cartItemRepository.save(item);
+
+      return genenateReturnObject(200, addItem, '');
+    } catch (e) {
+      return genenateReturnObject(e.statusCode, {}, (e as Error).message);
     }
-    const item = new CartItem();
-    item.cart = cartId;
-    item.inventory = addItemDto.inventoryId;
-    item.quantity = addItemDto.quantity;
-
-    const addItem = await this.cartItemRepository.save(item);
-
-    return await this.findOne(addItem.cart);
   }
 
   async findAll(options: IPaginationOptions): Promise<object> {
-    const query = this.cartRepository.createQueryBuilder('cart');
-    return { data: await paginate<Cart>(query, options) };
+    try {
+      const query = this.cartRepository.createQueryBuilder('cart');
+      const data = await paginate<Cart>(query, options);
+      return genenateReturnObject(200, data, '');
+    } catch (e) {
+      return genenateReturnObject(e.statusCode, {}, (e as Error).message);
+    }
   }
 
   async findOne(cartId: number) {
-    const cart = await this.cartRepository.findOneBy({ id: cartId });
-    if (!cart) {
-      return { error: { message: 'Cart not found.' } };
+    try {
+      const cart = await this.cartRepository.findOneBy({ id: cartId });
+      if (!cart) {
+        return genenateReturnObject(404, {}, 'Cart not found.');
+      }
+      // const customer = await this.customersService.findOne(cart.customer);
+
+      const items = await this.cartItemRepository.find({
+        where: {
+          cart: Equal(cart.id),
+        },
+      });
+      cart['items'] = items;
+      return genenateReturnObject(200, cart);
+    } catch (e) {
+      return genenateReturnObject(e.statusCode, {}, (e as Error).message);
     }
-    const customer = await this.customersService.findOne(cart.customer);
-
-    const items = await this.cartItemRepository.find({
-      select: {
-        inventory: true,
-        quantity: true,
-      },
-      where: {
-        cart: cartId,
-      },
-    });
-
-    return { data: { cart, customer, items } };
   }
 
   async updateItem(
@@ -118,29 +144,29 @@ export class CartService {
     updateItemDto: UpdateItemDto,
     request: Request,
   ): Promise<object> {
-    if (!(await this.cartRepository.findOneBy({ id: cartId }))) {
-      return { error: { message: 'Cart not found.' } };
-    }
-    if (!(await this.inventoryService.findOne(itemId))) {
-      return { error: { message: 'Can not find product in inventory.' } };
-    }
+    try {
+      const result = await this.isItemInCartValidtor(cartId, itemId);
+      if (result['statusCode'] != 200) {
+        return result;
+      }
+      const itemInCart = result['data'];
 
-    const isItemInCart = await this.cartItemRepository.findOneBy({
-      cart: cartId,
-      inventory: itemId,
-    });
-    if (isItemInCart) {
-      await this.cartItemRepository.update(isItemInCart.id, {
-        updateBy: request['user'].sub,
-        updateAt: new Date(),
+      if (updateItemDto.quantity > itemInCart.quantity) {
+        return genenateReturnObject(
+          404,
+          {},
+          'Inventory has less product than update quantity.',
+        );
+      }
+
+      await this.cartItemRepository.update(itemInCart.id, {
+        updatedBy: request['user'].sub,
       });
-      const updateItem = await this.cartItemRepository.update(
-        isItemInCart.id,
-        updateItemDto,
-      );
-      return { data: updateItem };
+      await this.cartItemRepository.update(itemInCart.id, updateItemDto);
+      return this.findOne(cartId);
+    } catch (e) {
+      return genenateReturnObject(e.statusCode, {}, (e as Error).message);
     }
-    return { error: { message: 'Something went wrong.' } };
   }
 
   async updateCart(
@@ -148,21 +174,121 @@ export class CartService {
     updateCartDto: UpdateCartDto,
     request: Request,
   ): Promise<object> {
-    const cart = await this.cartRepository.findOneBy({ id: cartId });
-    if (!cart) {
-      return { error: { message: 'Cart not found.' } };
-    }
-    console.log(updateCartDto);
-    await this.cartRepository.update(cartId, updateCartDto);
-    await this.cartRepository.update(cartId, {
-      updateBy: request['user'].sub,
-      updateAt: new Date(),
-    });
+    try {
+      const cart = await this.cartRepository.findOneBy({ id: cartId });
+      if (!cart) {
+        return genenateReturnObject(404, {}, 'Cart not found.');
+      }
+      if (!updateCartDto.customerId) {
+        return genenateReturnObject(400, {}, 'Nothing to update.');
+      }
+      /*
+      Cart 1 to 1 Customer,
+      update can break this law
+      And here got error TS2559
+       */
 
-    return await this.findOne(cartId);
+      // await this.cartRepository.update(cartId, updateCartDto);
+      // await this.cartRepository.update(cartId, {
+      //   updatedBy: request['user'].sub,
+      // });
+      // return await this.findOne(cartId);
+      const data = {
+        id: request['user'].sub,
+      };
+      return genenateReturnObject(
+        201,
+        data,
+        'Not implement cause data relationship.',
+      );
+    } catch (e) {
+      return genenateReturnObject(e.statusCode, {}, (e as Error).message);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cart`;
+  async remove(cartId: number, itemId: number, request: Request) {
+    try {
+      const result = await this.isItemInCartValidtor(cartId, itemId);
+      if (result['statusCode'] != 200) {
+        return result;
+      }
+
+      const itemInCart = result['data'];
+      await this.cartItemRepository.update(itemInCart.id, {
+        deletedBy: request['user'].sub,
+      });
+      await this.cartItemRepository
+        .createQueryBuilder()
+        .softDelete()
+        .where('id = :id', { id: itemInCart.id })
+        .execute();
+      return this.findOne(cartId);
+    } catch (e) {
+      return genenateReturnObject(e.statusCode, {}, (e as Error).message);
+    }
+  }
+
+  async restore(cartId: number, itemId: number, request: Request) {
+    try {
+      const item = await this.cartItemRepository.findOne({
+        where: {
+          cart: Equal(cartId),
+          inventory: Equal(itemId),
+          deletedAt: Not(IsNull()),
+        },
+        withDeleted: true,
+      });
+      if (!item) {
+        return genenateReturnObject(
+          404,
+          {},
+          'Item marked as delete not found in cart.',
+        );
+      }
+      await this.cartItemRepository.update(item.id, {
+        updatedBy: request['user'].sub,
+      });
+      await this.cartItemRepository
+        .createQueryBuilder()
+        .restore()
+        .where('id = :id', { id: item.id })
+        .execute();
+      return this.findOne(cartId);
+    } catch (e) {
+      return genenateReturnObject(e.statusCode, {}, (e as Error).message);
+    }
+  }
+
+  /*
+  Validate data logic
+   */
+  async isItemInCartValidtor(cartId: number, itemId: number): Promise<object> {
+    try {
+      const isCartExist = await this.cartRepository.findOneBy({ id: cartId });
+      if (!isCartExist) {
+        return genenateReturnObject(404, {}, 'Cart not found.');
+      }
+
+      const isItemExist = await this.inventoryService.findOne(itemId);
+      if (isItemExist['statusCode'] != 200) {
+        return genenateReturnObject(
+          404,
+          {},
+          'Can not find product in inventory.',
+        );
+      }
+
+      const isItemInCart = await this.cartItemRepository.findOneBy({
+        cart: Equal(isCartExist.id),
+        inventory: Equal(isItemExist['data'].id),
+      });
+
+      if (!isItemInCart) {
+        return genenateReturnObject(404, {}, 'Product not found in cart.');
+      }
+      return genenateReturnObject(200, isItemInCart);
+    } catch (e) {
+      return genenateReturnObject(e.statusCode, {}, (e as Error).message);
+    }
   }
 }
