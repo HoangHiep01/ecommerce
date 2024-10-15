46c07655e1a80a61c9273eda2abbe9b544ae274f
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-15 13:56:12 +0700

    feat(): apply logger with winston

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import { instance } from './logger/winston.logger';
 import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      instance: instance,
+    }),
+  });
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
 
-  setupSwagger(app);
-
   app.setGlobalPrefix('api/v1');
+  setupSwagger(app);
 
   await app.listen(configService.get<number>('port'));
 }
