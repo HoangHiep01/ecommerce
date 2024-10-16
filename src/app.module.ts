fce0676528f21045409aef9fff50d18e42649275
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-16 10:16:09 +0700

    feat(import,provide): implement rate limit request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module, Logger } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataSource } from 'typeorm';
@@ -13,11 +15,13 @@ import { InventoryModule } from './modules/inventory/inventory.module';
 import { CartModule } from './modules/cart/cart.module';
 import { OrderModule } from './modules/order/order.module';
 import { ReportModule } from './modules/report/report.module';
+import { thottleAsyncConfig } from './config/rate-limit.config';
 import configuration from './config/configuration';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    ThrottlerModule.forRootAsync(thottleAsyncConfig),
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local'],
       ignoreEnvFile: false,
@@ -34,7 +38,14 @@ import configuration from './config/configuration';
     ReportModule,
   ],
   controllers: [AppController],
-  providers: [AppService, Logger],
+  providers: [
+    AppService,
+    Logger,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {
   constructor(
