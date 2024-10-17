6c8458d1b157aeb9dd597144ebd6ca52f7acea40
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-17 09:16:25 +0700

    feat(): apply logger

--- src/modules/report/report.module.ts
+++ src/modules/report/report.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
+import { OrderModule } from '../order/order.module';
+import { OrderItem } from '../order//entities/order-item.entity';
+import { Order } from '../order//entities/order.entity';
 
 @Module({
-  providers: [ReportService],
+  imports: [OrderModule, TypeOrmModule.forFeature([Order, OrderItem])],
   controllers: [ReportController],
+  providers: [ReportService, Logger],
+  exports: [ReportService],
 })
 export class ReportModule {}
