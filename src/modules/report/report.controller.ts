70f3ff2cae9e799f8ec4d9d7303f792e23735b3d
HoangHiep <nhoanghiep2001@gmail.com>
2024-10-18 09:31:37 +0700

    feat(): declare revenue route

--- src/modules/report/report.controller.ts
+++ src/modules/report/report.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { ReportService } from './report.service';
 import { ApiDocument } from '../../decorators/document.decorator';
@@ -19,18 +19,28 @@ export class ReportController {
   }
 
   @ApiDocument('Get sales revenue by day', 'Total income of the day')
-  @Get('revenue/day')
-  dayRevenue() {}
+  @Get('revenue/day/:day/:month/:year')
+  dayRevenue(
+    @Param('day') day: string,
+    @Param('month') month: string,
+    @Param('year') year: string,
+  ) {
+    return this.reportService.getDayRevenue(day, month, year);
+  }
 
-  @ApiDocument('Get sales revenue by week', 'Total income of the week')
-  @Get('revenue/week')
-  weekRevenue() {}
+  // @ApiDocument('Get sales revenue by week', 'Total income of the week')
+  // @Get('revenue/week/:day/:month/:year')
+  // weekRevenue(@Param('day') day: string, @Param('month') month: string, @Param('year') year: string) {}
 
   @ApiDocument('Get sales revenue by month', 'Total income of the month')
-  @Get('revenue/month')
-  monthRevenue() {}
+  @Get('revenue/:month/:year')
+  monthRevenue(@Param('month') month: string, @Param('year') year: string) {
+    return this.reportService.getMonthRevenue(month, year);
+  }
 
   @ApiDocument('Get sales revenue by year', 'Total income of the year')
-  @Get('revenue/year')
-  yearRevenue() {}
+  @Get('revenue/:year')
+  yearRevenue(@Param('year') year: string) {
+    return this.reportService.getYearRevenue(year);
+  }
 }
